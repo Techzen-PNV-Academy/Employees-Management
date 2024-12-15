ade90db2c9071f0531aea291c4a0c52b88f81470
user_name <user_email>
2024-12-16 00:41:55 +0700

    Done bai 7

--- src/main/java/com/student/pnv/Controller/DepartmentController.java
+++ src/main/java/com/student/pnv/Controller/DepartmentController.java
@@ -1,5 +1,7 @@
 package com.student.pnv.Controller;
 
+import com.student.pnv.dto.ApiResponse;
+import com.student.pnv.service.IDepartmentService;
 import com.student.pnv.util.JSonResponse;
 import com.student.pnv.exception.AppException;
 import com.student.pnv.exception.ErrorCode;
@@ -17,60 +19,36 @@ import java.util.List;
 @RestController
 @CrossOrigin(value = "http://localhost:5173")
 @RequestMapping("/department")
-@AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class DepartmentController {
-    private List<Department> departments = new ArrayList<>(
-            Arrays.asList(
-                    new Department(1, "Quản lý"),
-                    new Department(2, "Kế toán"),
-                    new Department(3, "Sale Marketing"),
-                    new Department(4, "Sản xuất")
-            )
-    );
+    IDepartmentService departmentService;
 
-    @GetMapping
-    public ResponseEntity<?> getDepartment() {
-        return JSonResponse.ok(departments);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getDepartmentById(@PathVariable("id") int id) {
-        return departments.stream()
-                .filter(d -> d.getId() == id)
-                .findFirst()
-                .map(JSonResponse::ok)
-                .orElseThrow(() -> new AppException(ErrorCode.DEPARTMENT_NOT_EXIST));
+    public DepartmentController(IDepartmentService departmentService) {
+        this.departmentService = departmentService;
     }
 
     @PostMapping
-    public ResponseEntity<?> addDepart(@RequestBody Department department) {
-        department.setId((int) (Math.random()*100000000));
-        departments.add(department);
-        return JSonResponse.created(department);
+    public ResponseEntity<ApiResponse<Department>> createDepartment(@RequestBody Department newDepartment) {
+        return JSonResponse.created(departmentService.saveDepartment(newDepartment));
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<?> updateDepart(@PathVariable("id") int id, @RequestBody Department department) {
-        return departments.stream()
-                .filter(d -> d.getId() == id)
-                .findFirst()
-                .map(d -> {
-                    d.setName(department.getName());
-                    return JSonResponse.ok(d);
-                })
-                .orElseThrow(() -> new AppException(ErrorCode.DEPARTMENT_NOT_EXIST));
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<Department>>> getDepartments() {
+        return JSonResponse.ok(departmentService.getAllDepartments());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<Department>> getDepartmentById(@PathVariable Integer id) {
+        return JSonResponse.ok(departmentService.getDepartment(id));
+    }
+
+    @PutMapping
+    public ResponseEntity<ApiResponse<Department>> updateDepartment(@RequestBody Department updatedDepartment) {
+        return JSonResponse.ok(departmentService.updateDepartment(updatedDepartment));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteDepart(@PathVariable("id") int id) {
-        return departments.stream()
-                .filter(d -> d.getId() == id)
-                .findFirst()
-                .map(d -> {
-                    departments.remove(d);
-                    return JSonResponse.noContent();
-                })
-                .orElseThrow(() -> new AppException(ErrorCode.DEPARTMENT_NOT_EXIST));
+    public ResponseEntity<ApiResponse<Department>> deleteDepartment(@PathVariable int id) {
+        return JSonResponse.ok(departmentService.deleteDepartment(id));
     }
 }
